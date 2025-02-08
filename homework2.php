f418f528fbec15e40fa353d4f97b8476bf77bcce
CoolDude657898 <mjm4739@gmail.com>
2025-02-08 03:04:16 -0500

    Fix: Added semicolons and quotation marks for each echo

--- homework2.php
+++ homework2.php
@@ -1,10 +1,9 @@
 <?php  
-    echo <html>
-        echo <head>
-
-        echo </head>
-        echo <body>
-            echo <h1>Test</h1>
-        echo </body>
-    echo </html>
+    echo "<html>";
+    echo "<head>";
+    echo "</head>";
+    echo "<body>";
+    echo "<h1>Test</h1>";
+    echo "</body>";
+    echo "</html>";
 ?>
